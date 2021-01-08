11b3f5923bba1b654de0fa1759e5b18904e34434
LeeZeitz <andrew.l.zeitz@gmail.com>
2021-01-07 20:48:29 -0800

    add pause menu to GeneralLevel

--- Levels/General Level.gd
+++ Levels/General Level.gd
@@ -1,1 +1,11 @@
 extends Node
+
+
+onready var pauseMenu = $PauseMenu
+onready var playerCamera = $Player/Camera2D
+
+func _input(event):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().paused = true
+		pauseMenu.set_global_position(playerCamera.get_camera_position())
+		pauseMenu.show()
